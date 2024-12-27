2f6f59a7407a3de0cc10e985384981e13aa7faa0
amitvermaofficial <amitvns159@gmail.com>
2024-12-27 15:15:44 +0530

    Changed Class name SinglyLinkedList

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,7 +1,6 @@
 package LinkedList;
 
-
-class SinglyLinkedList {
+public class SinglyLinkedList {
     Node head;
     Node tail;
     int size;
@@ -102,11 +101,11 @@ class Node {
     }
 }
 
-public class Implementation {
-    public static void main(String[] args) {
-        Node a = new Node(1);
-        Node b = new Node(2);
-        System.out.println(a.val);
-        System.out.println(b.next);
-    }
-}
+// class Implementation {
+//     public static void main(String[] args) {
+//         Node a = new Node(1);
+//         Node b = new Node(2);
+//         System.out.println(a.val);
+//         System.out.println(b.next);
+//     }
+// }
