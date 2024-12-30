3afa3ba5cd22a6d8a0f48614b8e4d7b53433fa5e
amitvermaofficial <amitvns159@gmail.com>
2024-12-30 23:03:21 +0530

    LeetCode 86.

---
+++ Problems/PartitionList.java
@@ -0,0 +1,46 @@
+package LinkedList.Problems;
+
+import LinkedList.SinglyLinkedList;
+import java.util.Scanner;
+import LinkedList.Node;
+
+public class PartitionList {
+    public Node partition(Node head, int x) {
+        if (head==null) return head;
+        Node a = new Node(-1);   // dummy node
+        Node b = new Node(-1);   // dummy node
+        Node tempA = a;
+        Node tempB = b;
+        Node temp = head;
+        while (temp != null) {
+            if (temp.val < x) {
+                tempA.next = temp;
+                tempA = tempA.next;
+            } else {
+                tempB.next = temp;
+                tempB = tempB.next;
+            }
+            temp = temp.next;
+        }  
+        tempB.next = null; // coz last node       
+        tempA.next = b.next; // connect list a with list b
+        return a.next; // return the head of the new list
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SinglyLinkedList list = new SinglyLinkedList();
+        while (sc.hasNextInt()) {
+            list.insertAtTail(sc.nextInt());
+        }
+        System.out.print("Enter the value of x: ");
+        sc.nextLine();   // to consume String input
+        sc.nextLine(); // to consume nextLine Character/
+        int x = sc.nextInt();
+
+        PartitionList obj = new PartitionList();
+        Node head = obj.partition(list.head, x);
+        list.display(head);
+        sc.close();
+    }
+}
